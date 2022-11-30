0ecb7589064d0e341e3d30f0a57f60d84f11128b
abhishekmalakar <malakar.abh@gmail.com>
2022-11-30 23:10:48 +0530

    Changes made

--- src/main/java/org/example/Honda.java
+++ src/main/java/org/example/Honda.java
@@ -3,6 +3,6 @@ package org.example;
 public class Honda extends Bike{
     @Override
     void run() {
-        System.out.println("Running Safely");
+        System.out.println("Running Safely !!");
     }
 }
